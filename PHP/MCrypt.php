609819db4b84e1a63b9cdbce3e0a4dfc2c4be5e9
Pavel Kosteko <poul.kg@gmail.com>
2014-05-16 14:35:51 +0600

    * added support for encrypting / decrypting binary files, preserving backward compatibility via parameter default values

--- PHP/MCrypt.php
+++ PHP/MCrypt.php
@@ -1,52 +1,66 @@
-<?php 
+<?php
 
 class MCrypt
 {
-        private $iv = 'fedcba9876543210'; #Same as in JAVA
-        private $key = '0123456789abcdef'; #Same as in JAVA
+    private $iv = 'fedcba9876543210'; #Same as in JAVA
+    private $key = '0123456789abcdef'; #Same as in JAVA
 
 
-        function __construct()
-        {
+    function __construct()
+    {
+    }
+
+    /**
+     * @param string $str
+     * @param bool $isBinary whether to encrypt as binary or not. Default is: false
+     * @return string Encrypted data
+     */
+    function encrypt($str, $isBinary = false)
+    {
+        $iv = $this->iv;
+        $str = $isBinary ? $str : utf8_decode($str);
+
+        $td = mcrypt_module_open('rijndael-128', ' ', 'cbc', $iv);
+
+        mcrypt_generic_init($td, $this->key, $iv);
+        $encrypted = mcrypt_generic($td, $str);
+
+        mcrypt_generic_deinit($td);
+        mcrypt_module_close($td);
+
+        return $isBinary ? $encrypted : bin2hex($encrypted);
+    }
+
+    /**
+     * @param string $code
+     * @param bool $isBinary whether to decrypt as binary or not. Default is: false
+     * @return string Decrypted data
+     */
+    function decrypt($code, $isBinary = false)
+    {
+        $code = $isBinary ? $code : $this->hex2bin($code);
+        $iv = $this->iv;
+
+        $td = mcrypt_module_open('rijndael-128', ' ', 'cbc', $iv);
+
+        mcrypt_generic_init($td, $this->key, $iv);
+        $decrypted = mdecrypt_generic($td, $code);
+
+        mcrypt_generic_deinit($td);
+        mcrypt_module_close($td);
+
+        return $isBinary ? trim($decrypted) : utf8_encode(trim($decrypted));
+    }
+
+    protected function hex2bin($hexdata)
+    {
+        $bindata = '';
+
+        for ($i = 0; $i < strlen($hexdata); $i += 2) {
+            $bindata .= chr(hexdec(substr($hexdata, $i, 2)));
         }
 
-        function encrypt($str) {
-          $iv = $this->iv;
-
-          $td = mcrypt_module_open('rijndael-128', ' ', 'cbc', $iv);
-
-          mcrypt_generic_init($td, $this->key, $iv);
-          $encrypted = mcrypt_generic($td, utf8_decode($str));
-
-          mcrypt_generic_deinit($td);
-          mcrypt_module_close($td);
-
-          return bin2hex($encrypted);
-        }
-
-        function decrypt($code) {
-          $code = $this->hex2bin($code);
-          $iv = $this->iv;
-
-          $td = mcrypt_module_open('rijndael-128', ' ', 'cbc', $iv);
-
-          mcrypt_generic_init($td, $this->key, $iv);
-          $decrypted = mdecrypt_generic($td, $code);
-
-          mcrypt_generic_deinit($td);
-          mcrypt_module_close($td);
-
-          return utf8_encode(trim($decrypted));
-        }
-
-        protected function hex2bin($hexdata) {
-          $bindata = '';
-
-          for ($i = 0; $i < strlen($hexdata); $i += 2) {
-                $bindata .= chr(hexdec(substr($hexdata, $i, 2)));
-          }
-
-          return $bindata;
-        }
+        return $bindata;
+    }
 
 }
